319f2dc37e84a9698c2ddbe6b8d408d2000d2733
Jason Lam <nyjasonlam@gmail.com>
2021-09-30 16:32:38 -0400

    installed mongoose, apollo

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const routes = require('./routes')
 
 // env var for deploy
 const PORT = process.env.PORT || 3001
@@ -6,6 +7,7 @@ const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
